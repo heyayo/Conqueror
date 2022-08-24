448352200b308a0fe3b56c1544a8dce8c5d83e64
222839K <222839K@mymail.nyp.edu.sg>
2022-08-24 11:12:33 +0800

    changed lv 4

--- Scenes/LevelFour.cpp
+++ Scenes/LevelFour.cpp
@@ -5,41 +5,41 @@
 #include "DeadSoul.hpp"
 #include "Wall.hpp"
 
-Physical* wall2[2];
-Door* toNextLevel1;
-Player* player1;
-Actor* enemies1[5];
-DeadSoul* speaker1;
+Physical* wall4[2];
+Door* toNextLevel4;
+Player* player4;
+Actor* enemies4[5];
+DeadSoul* speaker4;
 
 void LevelFour::LoadScene()
 {
     SetBG("SceneBG/stage_4.png", V2(1920, 1080));
-    toNextLevel1 = new Door;
-    toNextLevel1->Redirect(LEVELFIVE);
-    toNextLevel1->SetPosition(1200, 500);
+    toNextLevel4 = new Door;
+    toNextLevel4->Redirect(LEVELFIVE);
+    toNextLevel4->SetPosition(1200, 500);
 
-    player1 = new Player;
-    player1->SetPosition(100, 350);
+    player4 = new Player;
+    player4->SetPosition(100, 350);
 
-    enemies1[0] = new Skeleton;
-    enemies1[0]->SetPosition(850, 250);
-    enemies1[1] = new Skeleton;
-    enemies1[1]->SetPosition(950, 350);
-    enemies1[2] = new Skeleton;
-    enemies1[2]->SetPosition(1150, 600);
-    enemies1[3] = new Skeleton;
-    enemies1[3]->SetPosition(1150, 200);
-    enemies1[4] = new Skeleton;
-    enemies1[4]->SetPosition(900, 300);
+    enemies4[0] = new Skeleton;
+    enemies4[0]->SetPosition(850, 250);
+    enemies4[1] = new Skeleton;
+    enemies4[1]->SetPosition(950, 350);
+    enemies4[2] = new Skeleton;
+    enemies4[2]->SetPosition(1150, 600);
+    enemies4[3] = new Skeleton;
+    enemies4[3]->SetPosition(1150, 200);
+    enemies4[4] = new Skeleton;
+    enemies4[4]->SetPosition(900, 300);
 
-    wall2[0] = new Wall;
-    wall2[0]->SetCollisionSize(V2(650, 1000));
-    wall2[0]->SetPosition(840, 780);
-    wall2[0]->Init(BLACK, V2(350, 580));
-    wall2[1] = new Wall;
-    wall2[1]->SetCollisionSize(V2(180, 240));
-    wall2[1]->SetPosition(840, 120);
-    wall2[1]->Init(GREEN, V2(330, 220));
+    wall4[0] = new Wall;
+    wall4[0]->SetCollisionSize(V2(650, 1000));
+    wall4[0]->SetPosition(840, 780);
+    wall4[0]->Init(BLACK, V2(350, 580));
+    wall4[1] = new Wall;
+    wall4[1]->SetCollisionSize(V2(180, 240));
+    wall4[1]->SetPosition(840, 120);
+    wall4[1]->Init(GREEN, V2(330, 220));
     std::string m[6];
     m[0] = "RIDE WIFE";
     m[1] = "LIFE GOOD";
@@ -47,17 +47,17 @@ void LevelFour::LoadScene()
     m[3] = "KILL WIFE";
     m[4] = "WIFE GONE";
     m[5] = "REGRET";
-    speaker1 = new DeadSoul(m, 6);
-    speaker1->SetPosition(300, 350);
+    speaker4 = new DeadSoul(m, 6);
+    speaker4->SetPosition(300, 350);
 
-    AddPhysical(player1);
-    AddPhysical(toNextLevel1);
-    AddPhysical(wall2[0]);
-    AddPhysical(wall2[1]);
-    for (auto i : enemies1)
+    AddPhysical(player4);
+    AddPhysical(toNextLevel4);
+    AddPhysical(wall4[0]);
+    AddPhysical(wall4[1]);
+    for (auto i : enemies4)
     {
         AddPhysical(i);
         std::cout << i->GetPosition() << std::endl;
     }
-    AddPhysical(speaker1);
+    AddPhysical(speaker4);
 }
