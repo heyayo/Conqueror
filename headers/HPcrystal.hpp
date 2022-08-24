af4addbfe223678d52709523e4da2c7fd7e60ea1
greviouskills <111328287+greviouskills@users.noreply.github.com>
2022-08-24 13:42:48 +0800

    HP crystal

--- headers/HPcrystal.hpp
+++ headers/HPcrystal.hpp
@@ -1,1 +1,17 @@
 #pragma once
+#include "Physical.hpp"
+#include "Character.h"
+#include "Player.hpp"
+
+class HPcrystal : public Physical
+{
+
+        
+public:
+    HPcrystal();
+    ~HPcrystal();
+
+    void Update() override;
+    void Start() override;
+
+};
