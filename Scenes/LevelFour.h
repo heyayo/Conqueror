52b8b01426c34863afffa0bbb918b16e3e4dce68
MiloHoon <brainlessx3@gmail.com>
2022-08-24 10:30:33 +0800

    update level4

--- Scenes/LevelFour.h
+++ Scenes/LevelFour.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef PROTOTYPE_LEVELTWO_HPP
+#define PROTOTYPE_LEVELTWO_HPP
+
+#include "Scene.hpp"
+
+class LevelFour : public Scene
+{
+
+public:
+    void LoadScene() override;
+    void SceneUpdate() override;
+    void Collision() override;
+};
+
+#endif
